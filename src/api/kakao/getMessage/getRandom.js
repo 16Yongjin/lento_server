a1a09260bdaa3b1af17ef7010a10e48e95f54bf6
16Yongjin <yongjin0802@gmail.com>
2018-03-01 05:28:36 +0900

    food model

--- src/api/kakao/getMessage/getRandom.js
+++ src/api/kakao/getMessage/getRandom.js
@@ -1,17 +1,16 @@
-const Foods = require('api/foods/controller')
+const Food = require('api/foods/model')
 const { getMapUrl, getLongUrl } = require('./getMapUrl')
 const keyboard = require('./keyboard')
 
 const getRandom = async () => {
   try {
-    const { name, type, time, menu, lat, lng } = await Foods.random()
-    const location = (lat && lng) ? getLongUrl(lat, lng) : null
-    console.log(location, lat, lng)
+    const { _id, name, type, time, menu, lat, lng } = await Food.randomShortId()
     const message = {
       text: `${name} ${type ? `(${type})` : ''}
       ${ time ? `영업시간: ${time}` : '' }
       ${ menu ? `메뉴: ${menu}` : '' }
-      ${ location ? `위치: ${location}` : '' }`.split('\n').map(s => s.trim()).join('\n')
+      https://lento.in/foods/${_id}`
+        .split('\n').map(s => s.trim()).join('\n')
     }
     return { message, keyboard }
   }
