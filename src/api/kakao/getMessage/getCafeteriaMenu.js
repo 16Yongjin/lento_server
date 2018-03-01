b01459a5ea34ea6f7c7f92e38dae702415224793
16Yongjin <yongjin0802@gmail.com>
2018-03-02 00:43:26 +0900

    cafeteria time

--- src/api/kakao/getMessage/getCafeteriaMenu.js
+++ src/api/kakao/getMessage/getCafeteriaMenu.js
@@ -14,24 +14,26 @@ const matchAll = re => menu => {
 
 const getCafeteriaMenu = async (message) => {
   const [place, eatingTime] = message.split(' ')
-  const today = moment().add(9, 'h').format('YYYYMMDD')
-  const url = `https://webs.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=${today}&endDt=${today}&`
+  const today = moment().add(5, 'h')
+  const YYYYMMDD = today.format('YYYYMMDD')
+  const url = `https://webs.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=${YYYYMMDD}&endDt=${YYYYMMDD}&`
         + (place === '인문관' ? '&caf_id=h101' : '&caf_id=h102')
   const re = eatingTime === '점심' ? /중식.+?\d+원/g : /석식.+?\d+원/g;
   try {
     const html = await request(url)
     const $ = cheerio.load(html)
-
-    var menu = $('table').text().replace(/\s+/g, ' ');
-    return matchAll(re)(menu).map(match => {
+    let menu = $('table').text().replace(/\s+/g, ' ')
+    const result = matchAll(re)(menu).map(match => {
       return match[0]
         .replace(' Kcal', 'Kcal')
         .replace(/\s+/g, '\n')
         .replace(/(\d{2})(\d{2}~\d{2})(\d{2})/g, ' $1:$2:$3\n')
         .replace('Kcal', ' Kcal')
     })
-    .join('\n\n') || '오늘은 밥 안 나와요'
+    result.unshift(today.format('M월 D일'))
+    return result.join('\n\n') || '오늘은 밥 안 나와요'
   } catch (e) {
+    console.log(e)
     return '학식 가져오기에 실패했습니다.'
   }
 }
