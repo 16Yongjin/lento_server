1ee2b02034102d262a90f864928a1eb7fdf9d3e9
16Yongjin <yongjin0802@gmail.com>
2018-03-09 14:04:30 +0900

    cache cafeteria

--- dist/api/kakao/getMessage/keyboard.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    'type': 'buttons',
-    'buttons': ['식당 추천', '학식']
-};
-//# sourceMappingURL=keyboard.js.map
--- dist/configuration/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("config");
-exports.default = {
-    get: (key) => {
-        return process.env[key] || config_1.default[key];
-    }
-};
-//# sourceMappingURL=index.js.map
